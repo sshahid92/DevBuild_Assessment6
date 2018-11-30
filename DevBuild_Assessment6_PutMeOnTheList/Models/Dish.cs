aed0ebbee658e2aaf32051e9c699eda7034e9f88
Shah Shahid <shahshahid@quickenloans.com>
2018-11-30 16:12:19 -0500

    checking in what i have so far.

---
+++ DevBuild_Assessment6_PutMeOnTheList/Models/Dish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevBuild_Assessment6_PutMeOnTheList.Models
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DishName { get; set; }
+        public bool DishDescription { get; set; }
+        public string Options { get; set; }
+}
